5d72b9f0c3861e720b107559cffc522ede9f10d8
Mao <28206886+MAOGAMI@users.noreply.github.com>
2019-03-05 20:07:37 +0100

    initial commit

---
+++ src/main/java/pl/diet/fithelper/FitHelperApplication.java
@@ -0,0 +1,13 @@
+package pl.diet.fithelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/diet/fithelper/FitHelperApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.diet.fithelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FitHelperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
